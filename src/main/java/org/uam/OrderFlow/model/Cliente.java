a66aee1dde8a47054266745bec9d63d4fbf52463
Andrea Duarte <ajduarteg@uamv.edu.ni>
2026-02-22 14:40:22 -0600

    add anotaciones cliente

--- src/main/java/org/uam/OrderFlow/model/Cliente.java
+++ src/main/java/org/uam/OrderFlow/model/Cliente.java
@@ -14,28 +14,9 @@ import java.math.BigDecimal;
 @Getter
 @Setter
 public class Cliente extends EntityBase {
-
-    @NotBlank(message = "Primer nombre no puede estar vacío")
-    private String primerNombre;
-
-    private String segundoNombre;
-
-    @NotBlank(message = "Primer apellido no puede estar vacío")
-    private String primerApellido;
-
-    private String segundoApellido;
-
-    @Email(message = "El correo no es válido")
-    private String email;
-
-    @NotBlank(message = "Teléfono no puede estar vacío")
-    private String telefono;
-
-    @NotBlank(message = "Dirección no puede estar vacío")
-    private String direccion;
-
-    @CedulaValida(message = "La cédula no es válida")
-    private String cedula;
+    @OneToOne
+    @JoinColumn(name = "usuario_id")
+    private Usuario usuario;
 
     private String codigoCliente;
 
@@ -48,7 +29,4 @@ public class Cliente extends EntityBase {
     @Column(length = 500)
     private String observaciones;
 
-    @OneToOne
-    @JoinColumn(name = "usuario_id")
-    private Usuario usuario;
 }
