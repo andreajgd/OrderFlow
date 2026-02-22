ab449c1269db51beedf292cd8485a56fe9f1a61f
Andrea Duarte <ajduarteg@uamv.edu.ni>
2026-02-22 14:40:40 -0600

    add anotaciones empleado

--- src/main/java/org/uam/OrderFlow/model/Empleado.java
+++ src/main/java/org/uam/OrderFlow/model/Empleado.java
@@ -17,27 +17,9 @@ import java.time.LocalDate;
 @Setter
 public class Empleado extends EntityBase {
 
-    @NotBlank(message = "Primer nombre no puede estar vacío")
-    private String primerNombre;
-
-    private String segundoNombre;
-
-    @NotBlank(message = "Primer apellido no puede estar vacío")
-    private String primerApellido;
-
-    private String segundoApellido;
-
-    @Email(message = "El correo no es válido")
-    private String email;
-
-    @NotBlank(message = "Teléfono no puede estar vacío")
-    private String telefono;
-
-    @NotBlank(message = "Dirección no puede estar vacío")
-    private String direccion;
-
-    @CedulaValida(message = "La cédula no es válida")
-    private String cedula;
+    @OneToOne
+    @JoinColumn(name = "usuario_id")
+    private Usuario usuario;
 
     @Enumerated(EnumType.STRING)
     private CargoEmpleado cargo;
@@ -53,7 +35,4 @@ public class Empleado extends EntityBase {
     @Column(length = 500)
     private String observaciones;
 
-    @OneToOne
-    @JoinColumn(name = "usuario_id")
-    private Usuario usuario;
 }
