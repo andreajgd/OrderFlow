44de39942e16275b6dfb2f2a1c58b19637c31d2f
Andrea Duarte <ajduarteg@uamv.edu.ni>
2026-02-24 20:08:14 -0600

    add Cotizacion

--- src/main/java/org/uam/OrderFlow/model/Cotizacion.java
+++ src/main/java/org/uam/OrderFlow/model/Cotizacion.java
@@ -1,17 +1,39 @@
 package org.uam.OrderFlow.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.UUID;
+import java.time.LocalDateTime;
+import java.util.List;
 
-public class Cotizacion {
-    private UUID id;
+@Entity
+@Table(name = "cotizaciones")
+@Getter
+@Setter
+public class Cotizacion extends EntityBase{
+    @Column(unique = true)
     private String codigo;
-    private Cliente cliente;
-    private LocalDate fechaCreacion;
-    private LocalDate validezHasta;
-    private String tipoServicio;
+    @NotBlank(message = "El nombre no puede estar vacío.")
+    private String nombreCliente;
+    @Column(length = 1000)
+    private String descripcion;
+    private String contactoCliente;
+
+    @NotNull @Positive
+    private BigDecimal totalEstimadoUsd;
+    @NotNull @Positive
+    private BigDecimal totalEstimadoNio;
+    private LocalDateTime fechaCreacion;
+    private LocalDateTime validezHasta;
     private String estado;
-    private BigDecimal totalNio;
-    private BigDecimal totalUsd;
+
+    @OneToMany(mappedBy = "cotizacion", cascade = CascadeType.ALL)
+    private List<ProductoCotizacion> items;
 }
