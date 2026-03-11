8942f8cd93f90b2d003596905105923fd7f7b49d
Andrea Duarte <ajduarteg@uamv.edu.ni>
2026-03-11 14:48:42 -0600

    add Auditoria

---
+++ src/main/java/org/uam/OrderFlow/config/AuditorProvider.java
@@ -0,0 +1,14 @@
+package org.uam.OrderFlow.config;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component("auditorProvider")
+public class AuditorProvider implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of("SYSTEM");
+    }
+}
